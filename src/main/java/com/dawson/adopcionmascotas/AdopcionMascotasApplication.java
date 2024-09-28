5c7d08ffbcd1ac79e737b29318127be355e9aa32
Nathal Dawson <nathaldawsondiaz@gmail.com>
2024-09-28 16:43:57 -0300

    Initial commit

---
+++ src/main/java/com/dawson/adopcionmascotas/AdopcionMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.dawson.adopcionmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopcionMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopcionMascotasApplication.class, args);
+	}
+
+}
